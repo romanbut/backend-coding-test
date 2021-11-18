eabdd2bbde45ad944f110c69dca88806831980a3
romanbut <r.but@aimprosoft.com>
2021-11-18 12:16:06 +0200

    Applied eslint fixes. Added codestyle from google. 'Prepush' plugin added and configured

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
 const buildSchemas = require('./src/schemas');
-const app = require("./src/app");
+const app = require('./src/app');
 
 db.serialize(() => {
-    buildSchemas(db);
+  buildSchemas(db);
 
-    const server = app(db);
+  const server = app(db);
 
-    server.listen(port, () => logger.info(`App started and listening on port ${port}`));
+  server.listen(port, () => logger.info(`App started and listening on port ${port}`));
 });
--- src/util/logger.js
+++ src/util/logger.js
@@ -1,21 +1,21 @@
 const winston = require('winston');
 
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    defaultMeta: {service: 'rides-service'},
-    transports: [
-        new winston.transports.File({filename: 'logs/error.log', level: 'error'}),
-        new winston.transports.File({filename: 'logs/info.log', level: 'info'}),
-        new winston.transports.File({filename: 'logs/combined.log'}),
-        new winston.transports.Console({
-            level: 'info',
-            format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.simple()
-            )
-        })
-    ],
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: {service: 'rides-service'},
+  transports: [
+    new winston.transports.File({filename: 'logs/error.log', level: 'error'}),
+    new winston.transports.File({filename: 'logs/info.log', level: 'info'}),
+    new winston.transports.File({filename: 'logs/combined.log'}),
+    new winston.transports.Console({
+      level: 'info',
+      format: winston.format.combine(
+          winston.format.colorize(),
+          winston.format.simple(),
+      ),
+    }),
+  ],
 });
 
 module.exports = logger;
